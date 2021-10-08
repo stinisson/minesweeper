040eafb4845195bb8b73e7f298c6cf9fd8d07db2
stinisson <krikarlss@gmail.com>
2021-10-08 13:34:45 +0200

    Verify square values by printing board in terminal

--- kmines_hard.py
+++ kmines_hard.py
@@ -1,88 +1,109 @@
 import pyautogui
 import glob
 
-files = [int(f[-7:-4]) for f in glob.glob("screenshots/*.png")]
-if files:
-    latest_file_ending = max(files)
-    file_ending = str(latest_file_ending + 1).zfill(4)
-else:
-    file_ending = "0000"
+SQUARE_WIDTH = SQUARE_HEIGHT = 22
+# Upper left matrix from locator top left (36, 53)
+# Bottom right matrix from locator top left (695, 404)
+WIDTH = 695 - 36
+HEIGHT = 404 - 53
 
-im = pyautogui.screenshot()
-locator_location = pyautogui.locateOnScreen('img/locator.png', confidence=0.9)
+BOT_RIGHT = (695, 404)
 
-if locator_location is not None:
+ROWS = 16
+COLS = 30
 
-    # Start a new game
-    locatorpoint = pyautogui.center(locator_location)
-    pyautogui.click(locatorpoint.x, locatorpoint.y)
+PIXEL_X = 12
+PIXEL_Y = 17
 
-    # if game ended, quit game
+
+def new_game():
+    im = pyautogui.screenshot()
+    locator_location = pyautogui.locateOnScreen('img/locator.png', confidence=0.9)
+    if locator_location is not None:
+        locator_point = pyautogui.center(locator_location)
+        pyautogui.click(locator_point.x, locator_point.y)
+        return locator_location
+    else:
+        print("Couldn't locate 'New' button. Make sure it's visible on the screen")
+        return None
+
+
+def get_file_ending():
+    files = [int(f[-7:-4]) for f in glob.glob("screenshots/*.png")]
+    if files:
+        latest_file_ending = max(files)
+        file_ending = str(latest_file_ending + 1).zfill(4)
+    else:
+        file_ending = "0000"
+    return file_ending
+
+
+def open_first_square(TOP_LEFT):
+    rc00 = TOP_LEFT[0] + SQUARE_WIDTH / 2, TOP_LEFT[1] + SQUARE_HEIGHT / 2
+    pyautogui.click(rc00[0], rc00[1])
+    print("Clicked first square", rc00[0], rc00[1])
+
+    file_ending = get_file_ending()
+    return pyautogui.screenshot(f"screenshots/screenshot_{file_ending}.png",
+                               region=(TOP_LEFT[0], TOP_LEFT[1], WIDTH, HEIGHT))
+
+
+def game_ended():
     game_ended = pyautogui.locateOnScreen('img/game_ended.png', confidence=0.9)
     if game_ended:
         print("Oh noes, a mine got clicked! \n Game ended.")
         quit()
 
-    # Upper left matrix from locator top left (204, 67)
-    # Bottom right matrix from locator top left (528, 391)
-    #WIDTH = 528 - 204
-    #HEIGHT = 391 - 67
 
-    # Upper left matrix from locator top left (36, 53)
-    # Bottom right matrix from locator top left (695, 404)
-    WIDTH = 695 - 36
-    HEIGHT = 404 - 53
+locator_location = new_game()
+if locator_location is None:
+    quit()
 
-    TOP_LEFT = (locator_location.left + 36, locator_location.top + 53)
-    BOT_RIGHT = (695, 404)
+TOP_LEFT = (locator_location.left + 36, locator_location.top + 53)
 
-    SQUARE_WIDTH = SQUARE_HEIGHT = 22
-    rc00 = TOP_LEFT[0] + SQUARE_WIDTH/2, TOP_LEFT[1] + SQUARE_HEIGHT/2
-    pyautogui.click(rc00[0], rc00[1])
-    print("Clicked first square", rc00[0], rc00[1])
-    im2 = pyautogui.screenshot(f"screenshots/screenshot_{file_ending}.png", region=(TOP_LEFT[0], TOP_LEFT[1], WIDTH, HEIGHT))
+im = open_first_square(TOP_LEFT)
 
-    ROWS = 16
-    COLS = 30
+coordinates = {}
+for row in range(ROWS):
+    #print("")
+    #print("- " * 61)
+    for col in range(COLS):
+        print("| ", end="")
+        col_coord = PIXEL_X + col * SQUARE_WIDTH
+        row_coord = PIXEL_Y + row * SQUARE_HEIGHT
+        rgb_pixel = im.getpixel((col_coord, row_coord))
 
-    PIXEL_X = 12
-    PIXEL_Y = 17
+        position = f"rc{row}-{col}"
+        value = ""
+        valuex = ""
+        if rgb_pixel == (193, 193, 193) or rgb_pixel == (194, 194, 194) or rgb_pixel == (191, 191, 191) \
+                or rgb_pixel == (193, 193, 194) or rgb_pixel == (189, 189, 189) or rgb_pixel == (187, 187, 187):
+            value = "clicked"
+            valuex = "x "
+        elif rgb_pixel == (4, 4, 130):
+            value = "blue 1"
+            valuex = "1 "
+        elif rgb_pixel == (1, 129, 1):
+            value = "green 2"
+            valuex = "2 "
+        elif rgb_pixel == (128, 102, 0):
+            value = "gold 3"
+            valuex = "3 "
+        elif rgb_pixel == (105, 27, 88):
+            value = "purple 4"
+            valuex = "4 "
+        elif rgb_pixel == (181, 181, 181) or rgb_pixel == (183, 183, 183):
+            value = "unclicked"
+            valuex = "- "
+        else:
+            value = "UNKNOWN"
+            valuex = "Z "
+            print(rgb_pixel)
+        print(valuex, end="")
+        coordinates[position] = (rgb_pixel, value)
+    print("|")
 
-    coordinates = {}
+for coord in coordinates:
+    #print(coord, coordinates[coord])
+    pass
 
-    print("TOP LEFT", TOP_LEFT)
-    print("(row, col)")
-    for row in range(ROWS):
-        for col in range(COLS):
-            #col_coord = SQUARE_WIDTH//2 + col * SQUARE_WIDTH
-            #row_coord = SQUARE_HEIGHT//2 + row * SQUARE_HEIGHT
-
-            col_coord = PIXEL_X + col * SQUARE_WIDTH
-            row_coord = PIXEL_Y + row * SQUARE_HEIGHT
-
-            rgb_pixel = im2.getpixel((col_coord, row_coord))
-            #print(f"({row}, {col}): {rgb_pixel}")
-
-            position = f"rc{row}-{col}"
-            square = ""
-            if rgb_pixel == (193, 193, 193) or rgb_pixel == (194, 194, 194) or rgb_pixel == (191, 191, 191) \
-                    or rgb_pixel == (193, 193, 194) or rgb_pixel == (189, 189, 189) or rgb_pixel == (187, 187, 187):
-                square = "clicked"
-            elif rgb_pixel == (4, 4, 130):
-                square = "blue 1"
-            elif rgb_pixel == (1, 129, 1):
-                square = "green 2"
-            elif rgb_pixel == (128, 102, 0):
-                square = "gold 3"
-            elif rgb_pixel == (105, 27, 88):
-                square = "purple 4"
-            elif rgb_pixel == (181, 181, 181) or rgb_pixel == (183, 183, 183):
-                square = "unclicked"
-            else:
-                square = "UNKNOWN"
-            coordinates[position] = (rgb_pixel, square)
-
-    for coord in coordinates:
-        print(coord, coordinates[coord])
-else:
-    print("Could not locate minesweeper window.")
