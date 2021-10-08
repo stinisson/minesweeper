c2b8e27c555cc08eb00b30a099600308217cad90
stinisson <krikarlss@gmail.com>
2021-10-08 18:59:21 +0200

    Adjust treshold values of unclicked squares

--- kmines_hard.py
+++ kmines_hard.py
@@ -1,13 +1,14 @@
 import pyautogui
 import glob
+import time
 
 SQUARE_WIDTH = SQUARE_HEIGHT = 22
 # Upper left matrix from locator top left (36, 53)
 # Bottom right matrix from locator top left (695, 404)
-WIDTH = 695 - 36
-HEIGHT = 404 - 53
+WIDTH = 696 - 36
+HEIGHT = 405 - 53
 
-BOT_RIGHT = (695, 404)
+BOT_RIGHT = (696, 405)
 
 ROWS = 16
 COLS = 30
@@ -17,7 +18,6 @@ PIXEL_Y = 17
 
 
 def new_game():
-    im = pyautogui.screenshot()
     locator_location = pyautogui.locateOnScreen('img/locator.png', confidence=0.9)
     if locator_location is not None:
         locator_point = pyautogui.center(locator_location)
@@ -38,72 +38,77 @@ def get_file_ending():
     return file_ending
 
 
-def open_first_square(TOP_LEFT):
-    rc00 = TOP_LEFT[0] + SQUARE_WIDTH / 2, TOP_LEFT[1] + SQUARE_HEIGHT / 2
+def open_first_square(top_left):
+    rc00 = top_left[0] + SQUARE_WIDTH / 2, top_left[1] + SQUARE_HEIGHT / 2
     pyautogui.click(rc00[0], rc00[1])
-    print("Clicked first square", rc00[0], rc00[1])
-
     file_ending = get_file_ending()
     return pyautogui.screenshot(f"screenshots/screenshot_{file_ending}.png",
-                               region=(TOP_LEFT[0], TOP_LEFT[1], WIDTH, HEIGHT))
+                                region=(top_left[0], top_left[1], WIDTH, HEIGHT))
 
 
 def game_ended():
-    game_ended = pyautogui.locateOnScreen('img/game_ended.png', confidence=0.9)
-    if game_ended:
+    if pyautogui.locateOnScreen('img/game_ended.png', confidence=0.9):
         print("Oh noes, a mine got clicked! \n Game ended.")
         quit()
 
 
-locator_location = new_game()
-if locator_location is None:
-    quit()
+def value_in_range(rgb_value, threshold, offset):
+    """ Return True if the squares' rgb value is within the given threshold +- offset"""
+    for idx, value in enumerate(rgb_value):
+        if not (threshold[idx] - offset <= value <= threshold[idx] + offset):
+            return False
+    return True
 
-TOP_LEFT = (locator_location.left + 36, locator_location.top + 53)
 
-im = open_first_square(TOP_LEFT)
+def run_game():
+    locator_location = new_game()
+    if locator_location is None:
+        quit()
 
-coordinates = {}
-for row in range(ROWS):
-    #print("")
-    #print("- " * 61)
-    for col in range(COLS):
-        print("| ", end="")
-        col_coord = PIXEL_X + col * SQUARE_WIDTH
-        row_coord = PIXEL_Y + row * SQUARE_HEIGHT
-        rgb_pixel = im.getpixel((col_coord, row_coord))
+    top_left = (locator_location.left + 36, locator_location.top + 53)
+    im = open_first_square(top_left)
 
-        position = f"rc{row}-{col}"
-        value = ""
-        valuex = ""
-        if rgb_pixel == (193, 193, 193) or rgb_pixel == (194, 194, 194) or rgb_pixel == (191, 191, 191) \
-                or rgb_pixel == (193, 193, 194) or rgb_pixel == (189, 189, 189) or rgb_pixel == (187, 187, 187):
-            value = "clicked"
-            valuex = "x "
-        elif rgb_pixel == (4, 4, 130):
-            value = "blue 1"
-            valuex = "1 "
-        elif rgb_pixel == (1, 129, 1):
-            value = "green 2"
-            valuex = "2 "
-        elif rgb_pixel == (128, 102, 0):
-            value = "gold 3"
-            valuex = "3 "
-        elif rgb_pixel == (105, 27, 88):
-            value = "purple 4"
-            valuex = "4 "
-        elif rgb_pixel == (181, 181, 181) or rgb_pixel == (183, 183, 183):
-            value = "unclicked"
-            valuex = "- "
-        else:
-            value = "UNKNOWN"
-            valuex = "Z "
-            print(rgb_pixel)
-        print(valuex, end="")
-        coordinates[position] = (rgb_pixel, value)
-    print("|")
+    coordinates = {}
+    print("")
+    for row in range(ROWS):
+        for col in range(COLS):
+            print("| ", end="")
+            col_coord = PIXEL_X + col * SQUARE_WIDTH
+            row_coord = PIXEL_Y + row * SQUARE_HEIGHT
+            rgb_value = im.getpixel((col_coord, row_coord))
 
-for coord in coordinates:
-    #print(coord, coordinates[coord])
-    pass
+            col_coord_frame = 1 + col * SQUARE_WIDTH
+            row_coord_frame = 1 + row * SQUARE_HEIGHT
+            rgb_value_frame = im.getpixel((col_coord_frame, row_coord_frame))
 
+            position = f"rc{row}-{col}"
+            value = ""
+            if value_in_range(rgb_value_frame, (163, 163, 163), 3):
+                value = "-"  # not-clicked
+            elif value_in_range(rgb_value, (185, 185, 185), 15):
+                value = "x"  # clicked
+            elif value_in_range(rgb_value, (4, 4, 130), 2):
+                value = "1"
+            elif value_in_range(rgb_value, (1, 129, 1), 2):
+                value = "2"
+            elif value_in_range(rgb_value, (128, 102, 0), 2):
+                value = "3"
+            elif value_in_range(rgb_value, (105, 27, 88), 2):
+                value = "4"
+            else:
+                value = "U"  # unknown
+                print("Unknown rgb:", rgb_value)
+
+            coordinates[position] = (rgb_value, value)
+            print(value, end=" ")
+        print("|")
+
+    for coord in coordinates:
+        # print(coord, coordinates[coord])
+        pass
+
+
+if __name__ == '__main__':
+    for i in range(1):
+        run_game()
+        time.sleep(3)
