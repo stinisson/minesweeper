c299102e99dc3f83dcadb731495d79913a251c00
stinisson <krikarlss@gmail.com>
2021-10-16 17:10:05 +0200

    Cleanup

--- constants.py
+++ constants.py
@@ -3,5 +3,4 @@ COLS, ROWS = 30, 16
 SQUARE_WIDTH = SQUARE_HEIGHT = 22       # (setup_board_size_with_gimp_square_width_height.png)
 WIDTH, HEIGHT = COLS * SQUARE_WIDTH, ROWS * SQUARE_HEIGHT
 BOARD_LEFT_TOP = (34, 53)               # Must be measured (setup_board_size_with_gimp.png)
-PIXEL_X, PIXEL_Y = 12, 17               # Where to look for number color in square, assuming square size 22,
-
+PIXEL_X, PIXEL_Y = 12, 17               # Where to look for number color in square, assuming square size 22
