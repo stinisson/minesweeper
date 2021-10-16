70f1b1d11a0ba4d29896cd8bb9d38e52bc3fda36
stinisson <krikarlss@gmail.com>
2021-10-16 22:20:16 +0200

    Add debug alternative to main

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import time
 from kmines_hard import run_game
 
 
-def main():
-    for i in range(999):
-        run_game()
+def main(debug=False):
+    for i in range(1):
+        run_game(debug)
         time.sleep(3)
 
 
 if __name__ == '__main__':
-    main()
+    main(debug=False)
